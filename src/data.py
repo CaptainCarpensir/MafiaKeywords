da68eea8a1b728f65a0ef9550a733c901940cf7b
tyler-torr <tylertorrella@gmail.com>
2025-07-29 18:28:52 -0700

    Game 11 Keywords

--- src/data.py
+++ src/data.py
@@ -1,57 +1,78 @@
 imgs = {
-    # ROLES
+    # AUDIT
     "https://imgur.com/a/LtyoVV7 \nhttps://imgur.com/a/YncwTNB": ["audit"],
+    
+    # TOWN ROLES
     "https://imgur.com/a/cQOvA8t": ["town", "town roles"],
-    "https://imgur.com/a/xzGz1Ir": ["justice", "town justice", "apollo"],
-    "https://imgur.com/a/PKLkEmI": ["duelist", "duel", "stance", "high noon"],
-    "https://imgur.com/a/kMZpVQc": ["gunslinger", "hunt", "spray and pray"],
-    "https://imgur.com/a/vPPww2u": ["sharpshooter", "snipe", "public execution"],
-    "https://imgur.com/a/tGtolce": ["sheriff", "reputation", "deescalation"],
+    # -- Town Justice
+    "https://imgur.com/a/BX2l4Xh": ["justice", "town justice", "apollo"],
+    "https://imgur.com/a/5F4sxKK": ["duelist", "duel", "stance", "high noon", "fiora", "boothill"],
+    "https://imgur.com/a/9OeKCc6": ["gunslinger", "hunt", "spray and pray"],
+    "https://imgur.com/a/q28vbnG": ["sharpshooter", "snipe", "public execution"],
+    "https://imgur.com/a/qfnP75d": ["sheriff", "reputation", "arrest", "deescalation"],
+    # -- Town Discovery
     "https://imgur.com/a/IyL9fUE": ["discovery", "town discovery"],
     "https://imgur.com/a/PmpSke5": ["coroner", "autopsy report", "gather prints", "postmortem"],
     "https://imgur.com/a/RVXVNnN": ["gumshoe", "educated guess", "breakthrough"],
     "https://imgur.com/a/tqMkxr2": ["librarian", "database search", "archival query"],
     "https://imgur.com/a/a7mlUVb": ["lookout", "watch", "track"],
+    # -- Town Protection
     "https://imgur.com/a/QTFlJV0": ["protection", "town protection"],
     "https://imgur.com/a/SaDvi9h": ["chef", "delicious dish", "savory meal", "charcuterie", "base meal", "sweet meal", "stew", "cheesecake"],
     "https://imgur.com/a/T7NFkhX": ["doctor", "heal", "self-heal"],
     "https://imgur.com/a/IGxoLQp": ["prophet", "veil", "forsake"],
-    "https://imgur.com/a/JF3siOP": ["surgeon", "lifeline", "urgent practices", "miracle cure"],
+    "https://imgur.com/a/snrSJ2c": ["surgeon", "lifeline", "urgent practices", "miracle cure"],
+    # -- Town Support
     "https://imgur.com/a/h6D4a0q": ["support", "town support"],
     "https://imgur.com/a/w0vPk74": ["cia operative", "cia op", "crack dealer", "midnight climax", "work the night", "live another day"],
     "https://imgur.com/a/RoljtXt": ["courier", "calligraphist", "mail delivery", "final message", "mailman"],
     "https://imgur.com/a/22e4nEx": ["judge", "overrule", "midnight trial"],
     "https://imgur.com/a/rqEhlFu": ["medium", "oracle", "seance", "last chance", "archivist", "ethereal"],
+    # -- Town Multipurpose
     "https://imgur.com/a/iiG18AJ": ["multipurpose", "town multipurpose"],
     "https://imgur.com/a/Y6SMi0r": ["angler", "fisher", "reel"],
-    "https://imgur.com/a/Z1Uty9k": ["cyborg", "batteries", "power: on", "/query", "/secure", "/terminate", "/charge"],
-    "https://imgur.com/a/uBykfsM": ["director", "birdie", "hounder", "ladybug", "system reset"],
-    "https://imgur.com/a/KvGsLQ9": ["inventor", "inventive mind", "collective genius", "blueprint", "brainstorm"],
-    "https://imgur.com/a/POaMstF": ["mafia", "mafia roles"],
-    "https://imgur.com/a/iA62x4X": ["leader", "mafia leader"],
-    "https://imgur.com/a/R3CBkWs": ["godfather", "gf", "iron fist", "the red hand", "massacre"],
-    "https://imgur.com/a/ncgRNog": ["killing", "mafia killing"],
-    "https://imgur.com/a/yDlLvDD": ["assassin", "target", "assassinate", "ass", "lights out", "calling card"],
+    "https://imgur.com/a/lhVo9U8": ["cyborg", "batteries", "power: on", "/query", "/secure", "/terminate", "/charge"],
+    "https://imgur.com/a/BF2EtiA": ["director", "birdie", "hounder", "ladybug", "system reset"],
+    "https://imgur.com/a/GzO472c": ["inventor", "inventive mind", "collective genius", "blueprint", "brainstorm"],
+    
+    # MAFIA ROLES
+    "https://imgur.com/a/InPAkXl": ["mafia", "mafia roles"],
+    # -- Mafia Leader
+    "https://imgur.com/a/sklMbo5": ["leader", "mafia leader"],
+    "https://imgur.com/a/ZobXKak": ["godfather", "gf", "iron fist", "the red hand", "massacre"],
+    # -- Mafia Killing
+    "https://imgur.com/a/Fgkd5Zx": ["killing", "mafia killing"],
+    "# ***(This role isn't in this game!)***\n### The last time it was played, it was like this:\n https://imgur.com/a/yDlLvDD": ["assassin", "target", "assassinate", "ass", "lights out", "calling card"],
+    "https://imgur.com/a/8egLhBb": ["bomber", "plant", "detonate", "big red button"],
     "https://imgur.com/a/uKHIuB1": ["conductor", "hostage", "trolley problem", "tie up"],
     "https://imgur.com/a/GWj7Aw3": ["ventriloquist", "cut loose", "string up", "march"],
-    "https://imgur.com/a/YOkmvzg": ["disruption", "mafia disruption"],
-    "https://imgur.com/a/Pje2n5Y": ["cloner", "doppelganger", "friendly face", "alibi"],
-    "https://imgur.com/a/xNab9Ot": ["ransomist", "threaten", "equipment", "buyout", "intel", "betrayal", "burn the evidence"],
-    "https://imgur.com/a/3TiXTSW": ["spy", "1984", "backdoor", "wiretap", "deep state", "bypass security", "Agent"],
-    "https://imgur.com/a/TC3UZqW": ["neutral", "neutral roles"],
+    # -- Mafia Disruption
+    "https://imgur.com/a/wpPRnoo": ["disruption", "mafia disruption"],
+    "https://imgur.com/a/2QJh6Th": ["cloner", "doppelganger", "friendly face"],
+    "https://imgur.com/a/ADLYEI4": ["mortician", "alibi", "cremation"],
+    "# ***(This role isn't in this game!)***\n### The last time it was played, it was like this:\n https://imgur.com/a/xNab9Ot": ["ransomist", "threaten", "equipment", "buyout", "intel", "betrayal", "burn the evidence"],
+    "https://imgur.com/a/csW0CKg": ["spy", "1984", "backdoor", "wiretap", "deep state", "bypass security", "Agent"],
+    
+    # NEUTRAL ROLES
+    "https://imgur.com/a/9fOklmp": ["neutral", "neutral roles"],
+    # -- Neutral Independent
     "https://imgur.com/a/FVEZ4Kv": ["independent", "neutral independent"],
     "https://imgur.com/a/DWHJsjA": ["apprentice", "quick learner", "teacher", "student", "sidekick"],
     "https://imgur.com/a/6bOaI2q \nhttps://imgur.com/a/EQ1ii8b": ["gambler", "gambits", "low stakes", "high stakes", "call", "check", "raise"],
     "https://imgur.com/a/XyGheoQ": ["jester", "the last laugh", "prank", "clown", "honk honk", "joker", "jimbo"],
+    # -- Neutral Mercenary
     "https://imgur.com/a/PQsusjU": ["mercenary", "neutral mercenary"],
-    "https://imgur.com/a/YNGCV6x": ["drunk driver", "god's drunkest driver", "collateral damage", "booze n cruise", "disembark", "end of the road"],
-    "https://imgur.com/a/lnSSDKP": ["executioner", "nemesis", "lifelong grudge", "tie the noose", "vilify", "darius"],
+    "https://imgur.com/a/7OAfdv8": ["drunk driver", "god's drunkest driver", "collateral damage", "booze n cruise", "disembark", "end of the road"],
+    "https://imgur.com/a/WjcxADg": ["executioner", "nemesis", "lifelong grudge", "tie the noose", "vilify", "darius"],
+    # -- Neutral Chaos
     "https://imgur.com/a/HyGIlp7": ["chaos", "neutral chaos", "jevil"],
-    "https://imgur.com/a/5RB5R6Q": ["hypnotist", "hypnotize", "power of suggestion"],
-    "https://imgur.com/a/OGb16LM": ["poltergeist", "ratatouille", "possession", "ghostwrite"],
+    "https://imgur.com/a/P3lTqPS": ["hypnotist", "hypnotize", "power of suggestion"],
+    "https://imgur.com/a/8a2Nxes": ["poltergeist", "ratatouille", "possession", "ghostwrite"],
+    # -- Neutral Destruction
     "https://imgur.com/a/jw9pMOE": ["neutral destruction", "destruction", "nd"],
-    "https://imgur.com/a/n8IfFv2": ["serial killer", "sk", "psychotic tendencies", "murder"],
-    "https://imgur.com/a/Sa3ksQg": ["werewolf", "puppy", "dog", "full moon", "rend"],
+    "https://imgur.com/a/QzoU5fO": ["ringleader", "circus", "the show goes on", "the final act", "caine"],
+    "https://imgur.com/a/VmUoWr9 \nhttps://imgur.com/a/ONnEfxC \nhttps://imgur.com/a/adElriz": ["serial killer", "sk", "psychotic tendencies", "murder", "killing frenzy"],
+    "# ***(This role isn't in this game!)***\n### The last time it was played, it was like this:\n https://imgur.com/a/Sa3ksQg": ["werewolf", "puppy", "dog", "full moon", "rend"],
 
 
     # KEYWORDS
@@ -68,30 +89,30 @@ imgs = {
     "https://imgur.com/a/3l8dSGk \nhttps://imgur.com/a/oZncCMa": ["win conditions", "win"],
     "https://imgur.com/a/KUsMCKR": ["defense", "basic kill", "basic defense", "powerful kill", "powerful defense", "obliteration", "invincibility"],
     "https://imgur.com/a/3fTotXI": ["alert", "discovery", "announcement"],
-    "https://imgur.com/a/zAJ1ElA": ["witness", "witness protection", "protection"],
+    "https://imgur.com/a/JWyDQuI": ["witness", "witness protection"],
     "https://imgur.com/a/mBFJwI6": ["attack", "kill"],
     "https://imgur.com/a/Psse57u": ["protect", "save", "safe"],
     "https://imgur.com/a/F6cC69H": ["status"],
     "https://imgur.com/a/S1upH6k": ["mark"],
-    "https://imgur.com/a/BEdrCLq": ["offer"],
+    "https://imgur.com/a/cZhHwXZ": ["offer"],
     "https://imgur.com/a/ITR1Vl9": ["item"],
     "https://imgur.com/a/tnRa643": ["resource"],
-    "https://imgur.com/a/FlbMICH": ["action order", "substage", "priority", "instant", "polling", "focus", "utility", "defensive", "offensive", "discovery"],
-    "https://imgur.com/a/jGSFza9": ["modifiers", "ability modifiers", "astral", "repeatable", "unstoppable", "special"],
+    "https://imgur.com/a/4UIwT0H": ["action order", "substage", "priority", "instant", "polling", "focus", "utility", "defensive", "offensive", "discovery"],
+    "https://imgur.com/a/v4mFOit": ["modifiers", "ability modifiers", "astral", "repeatable", "unstoppable", "special"],
     "https://imgur.com/a/YncwTNB": [], # NOTE: audit is already a keyword...
     "https://imgur.com/a/8jbd15K": ["roleblock", "block"],
-    "https://imgur.com/a/yTJRv7M": ["erase"],
+    "https://imgur.com/a/lhdYyik": ["erase"],
     "https://imgur.com/a/1Ul0Um0": ["disguise"],
-    "https://imgur.com/a/NYbilZv": ["ability fail", "alignment", "dawn", "dusk", "district", "follower", "retrieve", "new", "visit"],
+    "https://imgur.com/a/02yjdBO": ["ability fail", "alignment", "dawn", "dusk", "district", "follower", "retrieve", "new", "visit"],
     "https://imgur.com/a/RztkNC2": ["alerts", "consistent alerts"], # NOTE: too many things say alert wah
     "https://imgur.com/a/LThTaTG": ["discoveries", "consistent alerts"],
     "https://imgur.com/a/TgAUvZP": ["announcements", "consistent announcements"], # NOTE: TOO MANY THINGS SAY ANNOUNCEMENTS AND STUFF
     "https://imgur.com/a/JDUpllG": ["statuses", "addicted", "dummy", "guilty", "passenger", "vilified"],
-    "https://imgur.com/a/S0rdb0O": ["marks", "bonded", "damned", "ethereal", "fingerprinted", "nemesis", "on file", "possessed", "sighted", "targeted"],
-    "https://imgur.com/a/wdrLfWW": ["offers", "adrenaline", "a way out", "declare guilty", "duel", "magic powder"],
+    "https://imgur.com/a/EzlHnBA": ["marks", "bonded", "death role", "elated", "ethereal", "fingerprinted", "nemesis", "on file", "possessed", "sighted", "targeted"],
+    "https://imgur.com/a/o7P9990": ["offers", "adrenaline", "declare guilty", "duel", "invent", "magic powder"],
     "https://imgur.com/a/MZiY1qs": ["resources", "cash", "coins", "batteries", "extortions"],
     "https://imgur.com/a/1h57WIh": ["rank0", "rank 0 items", "megaphone", "pinata", "foggy monocle", "metal detector", "candy pipe", "prop gun", "mail parcel", "blindfold", "clown shoes"],
-    "https://imgur.com/a/Acgq0pW \nhttps://imgur.com/a/NWPaICv": ["rank1", "rank 1 items", "everclear shot", "medkit", "poison vial", "whetstone", "romeo's gun", "binoculars", "energy drink", "post stamp", "herbal tea", "monocle", "toolbox", "scalpel", "wiretap", "voodoo doll", "adrenaline", "bleach", "pager", "fake id", "sleep monitor", "last will", "red flag"],
+    "https://imgur.com/a/Acgq0pW \nhttps://imgur.com/a/jKu9obb": ["rank1", "rank 1 items", "everclear shot", "medkit", "poison vial", "whetstone", "romeo's gun", "binoculars", "energy drink", "post stamp", "herbal tea", "monocle", "toolbox", "scalpel", "wiretap", "voodoo doll", "adrenaline", "bleach", "pager", "fake id", "sleep monitor", "last will", "red flag"],
     "https://imgur.com/a/sLD0sY1": ["rank2", "rank 2 items", "handgun", "magic powder", "security camera", "smoke grenade", "bear trap", "extra ballots", "fishing rod", "priority mail", "bath bomb", "pristine monocle", "inheritance", "swiss army knife", "barricade"],
     "https://imgur.com/a/bSDSJzP": ["rank3", "rank 3 items", "sniper rifle", "master cookbook", "skeleton key", "tracking device", "philosopher's stone", "heroin", "rabbit's foot", "genie lamp", "visibility cloak", "pocketwatch"],
     "https://imgur.com/a/gRsCbwb": ["rank4", "rank 4 items", "second life", "steroids", "machine gun", "dominion", "pocket rat"],
@@ -99,6 +120,7 @@ imgs = {
     # FUN THINGS
     "https://imgur.com/a/Alcn8bl": ["bissy", "nyan", "toxic yaoi"],
     "https://imgur.com/a/pn6Oygi": ["mafiakeywords"],
+    "https://imgur.com/a/tZeFlo9": ["????"],
     """The missile knows where it is at all times. It knows this because it knows where it isn't, by subtracting where it is, from where it isn't, or where it isn't, from where it is, whichever is greater, it obtains a difference, or deviation. The guidance sub-system uses deviations to generate corrective commands to drive the missile from a position where it is, to a position where it isn't, and arriving at a position where it wasn't, it now is. Consequently, the position where it is, is now the position that it wasn't, and it follows that the position where it was, is now the position that it isn't. In the event of the position that it is in is not the position that it wasn't, the system has required a variation. The variation being the difference between where the missile is, and where it wasn't. If variation is considered to be a significant factor, it too, may be corrected by the GEA. However, the missile must also know where it was. The missile guidance computance scenario works as follows: Because a variation has modified some of the information the missile has obtained, it is not sure just where it is, however it is sure where it isn't, within reason, and it knows where it was. It now subracts where it should be, from where it wasn't, or vice versa. By differentiating this from the algebraic sum og where it shouldn't be, and where it was. It is able to obtain a deviation, and a variation, which is called "air".""": ["missile"],
     "woost...": ["fore", "4", "74"],
     """Hi, I'm Quinnigan""": ["quinnigan"],
